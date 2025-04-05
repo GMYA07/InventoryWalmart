bfef7de4d6ab4e2246d95a0078765d42c38a0211
javi menjivar <carlos.menjivar7@catolica.edu.sv>
2025-04-05 15:47:58 -0600

    Desing: Abance de la interfaz de reporte

--- InventoryWalmart/viewReportes.cs
+++ InventoryWalmart/viewReportes.cs
@@ -11,18 +11,19 @@ using System.Windows.Forms;
 
 namespace InventoryWalmart
 {
-    public partial class dashboard : Form
+    public partial class viewReportes : Form
     {
-        public dashboard()
+        public viewReportes()
         {
             InitializeComponent();
         }
 
-        private void barAcciones_Paint(object sender, PaintEventArgs e)
+        private void viewReportes_Load(object sender, EventArgs e)
         {
 
         }
 
+        //Codigo q nos ayuda con la administrasion de la barra de arriba y mover la ventana.
         //Drag Form
         [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
         private extern static void ReleaseCapture();
@@ -60,60 +61,16 @@ namespace InventoryWalmart
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
         private void btnInicio_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void btnDevoluciones_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dashboard_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel7_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panelBeneficiosReco_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panelDescuentos_Paint(object sender, PaintEventArgs e)
-        {
-            
-        }
-
-        private void panelBeneficiosReco_Click(object sender, EventArgs e)
-        {
-            viewBenefitsRewards viewBenefitsRewards = new viewBenefitsRewards();
+            dashboard dashboard = new dashboard();
             this.Hide();
-            viewBenefitsRewards.Show();
+            dashboard.Show();
         }
 
-        private void panelDescuentos_Click(object sender, EventArgs e)
+        private void barAcciones_Paint(object sender, PaintEventArgs e)
         {
-            viewDiscount viewDiscount = new viewDiscount();
-            this.Hide();
-            viewDiscount.Show();
-        }
 
-        private void btnReportes_Click(object sender, EventArgs e)
-        {
-            viewReportes viewReportes = new viewReportes();
-            this.Hide();
-            viewReportes.Show();
         }
     }
 }

161e4e3183c08c06121ff0f0b7c8b152791a12b3
javi menjivar <carlos.menjivar7@catolica.edu.sv>
2025-04-07 22:58:27 -0600

    Design: creacion por 4 vez generar reporte

---
+++ InventoryWalmart/FormGenerarReporte.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryWalmart
+{
+    public partial class FormGenerarReporte : Form
+    {
+        public FormGenerarReporte()
+        {
+            InitializeComponent();
+        }
+
+        //Drag Form
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void btnOcultar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panelBeneficiosReco_Click(object sender, EventArgs e)
+        {
+            viewBenefitsRewards viewBenefitsRewards = new viewBenefitsRewards();
+            this.Hide();
+            viewBenefitsRewards.Show();
+        }
+
+        private void panelDescuentos_Click(object sender, EventArgs e)
+        {
+            viewDiscount viewDiscount = new viewDiscount();
+            this.Hide();
+            viewDiscount.Show();
+        }
+        private void barAcciones_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnRestaurar.Visible = false;
+            btnMaximizar.Visible = true;
+        }
+
+
+
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnMaximizar.Visible = false;
+            btnRestaurar.Visible = true;
+
+        }
+    }
+}

5e26c885a0022c9db7f942206fe95cad6b5bf6a0
javi menjivar <carlos.menjivar7@catolica.edu.sv>
2025-05-03 22:32:40 -0600

    refactor: se creo un clase controlador para la logica de envio ala abse de datos. y se modifico la clase dao para que se use un prodedimineto almasenado

---
+++ InventoryWalmart/Controllers/RolesControllers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryWalmart.Database;
+
+namespace InventoryWalmart.Controllers
+{
+    internal class RolesControllers
+    {
+
+        public static void enviarRolBdd(Roles rol)
+        {
+            RolesDAO rolesDAO = new RolesDAO();
+            rolesDAO.InsertarRol(new Roles(0,rol.RoleName,rol.RoleDescription));
+        }
+
+
+    }
+}

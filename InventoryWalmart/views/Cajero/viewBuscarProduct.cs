534a40932cf835ef09ff363d5ab6c09f6e336c9c
17022004-klcv <150029678+17022004-klcv@users.noreply.github.com>
2025-05-31 08:42:36 -0600

    feat: atajos por teclado para buscar y refrescar tabla

--- InventoryWalmart/views/Cajero/viewBuscarProduct.cs
+++ InventoryWalmart/views/Cajero/viewBuscarProduct.cs
@@ -100,9 +100,10 @@ namespace InventoryWalmart.views.Cajero
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(inputBuscar.Text) && inputBuscar.Text.All(char.IsLetter))
+            if (!string.IsNullOrWhiteSpace(inputBuscar.Text) && inputBuscar.Text.All(c => char.IsLetter(c) || char.IsWhiteSpace(c)))
+
             {
-               
+
                 var producto = inputBuscar.Text;
                 var resultados = ProductDAO.obtenerProductoPorNombre(producto); // Lista de tuplas (Product, Categoria)
 
@@ -152,6 +153,28 @@ namespace InventoryWalmart.views.Cajero
         private void btnInicio_Click(object sender, EventArgs e)
         {
             ChangeView<indexCajero>();
+        }
+
+      
+
+        private void inputBuscar_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; // evita el sonido de "ding"
+                btnBuscar.PerformClick();  // simula clic en el botón
+            }
+
+        }
+
+        private void viewBuscarProduct_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F2)
+            {
+                e.SuppressKeyPress = true; // evita sonidos o comportamientos no deseados
+                inputBuscar.ResetText();
+               llenarTabla(); // llamas a un método que actualice la tabla
+            }
         }
     }
 }
